08e90669d5661845b4ef16c17e64b7d7b9821f2b
Emilio <e.arvonio@outlook.com>
2021-11-05 19:17:10 +0100

    Create Project

---
+++ Spok/SpokApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpokApp.swift
+//  Spok
+//
+//  Created by Emilio on 05/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SpokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
