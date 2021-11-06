7ed8377280bef5d72e9858d3dbe3a269c73d7284
Emilio <e.arvonio@outlook.com>
2021-11-06 16:41:06 +0100

    Add Counter View

---
+++ Spok/Scenes/Main/Views/Counter/CounterView.swift
@@ -0,0 +1,47 @@
+//
+//  CounterView.swift
+//  Spok
+//
+//  Created by Emilio on 06/11/21.
+//
+
+import SwiftUI
+
+struct CounterView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Image(systemName: "hand.thumbsup.fill")
+                .foregroundColor(Color.white)
+                .padding(.vertical)
+                .frame(width: 50, height: 120)
+                .font(.system(size: 50))
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Spot")
+                        .font(.title2)
+                        .bold()
+                        .foregroundColor(Color.white)
+                        .padding(.bottom, 1)
+                    Text("Fatti: 30")
+                        .font(.caption)
+                        .foregroundColor(Color.white)
+                }
+                Spacer()
+                Image(systemName: "arrow.forward.circle.fill")
+                    .foregroundColor(Color.white)
+                    .font(.title)
+            }
+        }
+        .padding(.horizontal, 20)
+        .frame(width: 210, height: 220)
+        .background(Color.yellow)
+        .cornerRadius(20)
+    }
+}
+
+struct CounterView_Previews: PreviewProvider {
+    static var previews: some View {
+        CounterView()
+            .previewLayout(.sizeThatFits)
+    }
+}
